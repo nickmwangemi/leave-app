2f634bb3a5f8744c89f71f084d40c89a9c8359f6
nickmwangemi <nickmwangemi@gmail.com>
2022-02-15 00:43:40 +0300

    Update settings for heroku deploy

--- leave_project/settings.py
+++ leave_project/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+
 """
 Django settings for leave_project project.
 
@@ -126,3 +128,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
