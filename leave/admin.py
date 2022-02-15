e40474cba083080f0470212006fde568c438d310
nickmwangemi <nickmwangemi@gmail.com>
2022-02-15 10:30:20 +0300

    Add list editable items in admin panel

--- leave/admin.py
+++ leave/admin.py
@@ -5,6 +5,8 @@ from .models import LeaveRequest
 
 class LeaveRequestAdmin(admin.ModelAdmin):
     list_display = ["name", "start_date", "end_date", "type_of_leave"]
+    list_editable = ["start_date", "end_date", "type_of_leave"]
+
 
 
 admin.site.register(LeaveRequest, LeaveRequestAdmin)
