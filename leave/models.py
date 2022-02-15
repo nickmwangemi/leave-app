c49daee2c21d2827d1029611b7d25af8ced1884a
nickmwangemi <nickmwangemi@gmail.com>
2022-02-15 10:44:31 +0300

    Create constraint to check that end date comes after start date

--- leave/models.py
+++ leave/models.py
@@ -1,8 +1,12 @@
 import datetime
+from distutils.command.check import check
+from unicodedata import name
 
 import numpy as np
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import F, Q
+from django.db.models.functions import Now
 from django.urls import reverse
 
 LEAVE_CHOICES = [
@@ -18,10 +22,6 @@ def validate_date_in_future(date):
     return date
 
 
-def validate_end_date_comes_after_start_date():
-    pass
-
-
 class LeaveRequest(models.Model):
     name = models.CharField(max_length=200)
     start_date = models.DateField(blank=False, validators=[validate_date_in_future])
@@ -29,6 +29,15 @@ class LeaveRequest(models.Model):
     type_of_leave = models.CharField(
         max_length=255, choices=LEAVE_CHOICES, default="NORMAL", db_index=True
     )
+
+    # Experimental feature at most. :)
+    # Adds a constraint to check whether end_date comes after start_date
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(start_date__lte=F("end_date")), name="correct_date"
+            )
+        ]
 
     def __str__(self):
         return f"{self.name} - {self.type_of_leave} leave request."
