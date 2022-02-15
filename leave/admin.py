3b530e4f8cb1b6705355512e6fe535e111e8566f
nickmwangemi <nickmwangemi@gmail.com>
2022-02-15 10:47:11 +0300

    Add leave day count to list editable items in admin panel

--- leave/admin.py
+++ leave/admin.py
@@ -4,7 +4,7 @@ from .models import LeaveRequest
 
 
 class LeaveRequestAdmin(admin.ModelAdmin):
-    list_display = ["name", "start_date", "end_date", "type_of_leave"]
+    list_display = ["name", "start_date", "end_date", "type_of_leave", "count_workdays"]
     list_editable = ["start_date", "end_date", "type_of_leave"]
 
 
